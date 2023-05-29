91e65243c9399b94b2ea34893b95be7af5849b98
MarelPup <marwane.elaouni@puppetsbox.com>
2023-05-29 13:56:12 +0200

    fright

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script language="typescript">
+  let line1 = "";
+  
+  const handleInput = (e) => {
+    line1 = e.target.value;
+  };
+
+
+  
+
+</script>
+
+<h1>Hello name</h1>
+
+<input type="text" name="line1" on:input={handleInput} />
+<p>{line1}</p>
+
+
+<div class=" 7-characters line line1 ">
+{#each Object.entries(line1) as c}
+  <span class="character" data-line1 = {c[0]} >{c[1]}</span>  
+{/each}
+</div>
+
+<style>
+
+    .character
+    .line{
+      display:flex;
+      flex-direction: row;
+    }
+
+    :global(body) {
+        background-color: #deadbeef;
+    }
+</style>
