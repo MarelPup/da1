ca767a49b13c3d1619e7902788f08b9fad6edd16
MarelPup <marwane.elaouni@puppetsbox.com>
2023-08-10 16:10:05 +0200

    prejourney

--- src/components/character/charater-row.svelte
+++ src/components/character/charater-row.svelte
@@ -1,27 +1,25 @@
 <script lang="ts">
     import Character from "./character.svelte";
     export let length: number;
+    export let value: string;
     //https://stackoverflow.com/questions/62698421/svelte-adding-a-class-to-a-div-doesnt-add-the-classes-css-to-div
     let line = "";
+
+    if (line.length > length) {
+      line = line.substring(0, length);
+    }
+
+    while (line.length < length) {
+      line += " ";
+    }
+
     for (let i = 0; i < length; i++) {
         line += " ";
     }
 
-    const handleInput = (e) => {
-        line = e.target.value;
-
-        if (line.length > length) {
-            line = line.substring(0, length);
-        }
-        while (line.length < length) {
-            line += " ";
-        }
-
-    };
-
 </script>
 
-<input type="text" name="line" on:input={handleInput} />
+
 <p>{line}</p>
 
 <div class=" 7-characters newline line">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,42 @@
 <script lang="ts">
   import CharaterRow from "../components/character/charater-row.svelte";
   let prop = 25;
+  let value1 = "";
+
+  const handleInput = (e) => {
+    let ttp = e.target.value;
+    value1 = "zsfv"; 
+  };
+
+
 </script>
 
-<h1>Hello name</h1>
-  <CharaterRow length={prop}/>
-<style>
 
+
+<div class="wrapper">
+  <textarea rows=3 cols=7 name="ttp" on:input={handleInput} />
+  <CharaterRow length={prop} value={value1} />
+</div>
+
+<style>
   :global(body, body *) {
     background-color: #272727;
     color: white;
     outline-color: white;
   }
+
+  .wrapper{
+    display: flex;
+    flex-direction: row;
+    
+  }
+  .wrapper{
+    flex: 1;
+  }
+  textarea {
+		position: absolute;
+		top: 0;
+		resize: none;
+	}
 
 </style>
