f448bfcbf0ccaf888f8288db6da092abf5bad6a3
MarelPup <marwane.elaouni@puppetsbox.com>
2023-08-17 19:04:02 +0200

    logic's all here , let's decorate

--- src/components/character/charater-row.svelte
+++ src/components/character/charater-row.svelte
@@ -1,29 +1,23 @@
 <script lang="ts">
     import Character from "./character.svelte";
-    export let length: number;
-    export let value: string;
+    export let lineLength: number;
+    export let line: string;
     //https://stackoverflow.com/questions/62698421/svelte-adding-a-class-to-a-div-doesnt-add-the-classes-css-to-div
-    let line = "";
-
-    if (line.length > length) {
-      line = line.substring(0, length);
-    }
-
-    while (line.length < length) {
+    
+    // line is only space until it's char
+    $: while (line.length < lineLength) {
       line += " ";
-    }
-
-    for (let i = 0; i < length; i++) {
-        line += " ";
+     
     }
 
 </script>
 
 
-<p>{line}</p>
+<p>{lineLength}</p>
 
-<div class=" 7-characters newline line">
-    {#each Object.entries(line) as c}
+<div class="7-characters newline line">
+    
+    {#each Object.entries(line) as c }
         <Character char={c[1]}/>
     {/each}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,28 @@
 <script lang="ts">
   import CharaterRow from "../components/character/charater-row.svelte";
-  let prop = 25;
-  let value1 = "";
-
+  let line1Length = 7;
+  let line2Length = 7;
+  let line3Length = 4;
+  let ttp = "";
+  let line1 = "";
+  let line2 = "";
+  let line3 = "";
   const handleInput = (e) => {
-    let ttp = e.target.value;
-    value1 = "zsfv"; 
+    ttp = e.target.value;
+    line1 = ttp.slice(0, line1Length);
+    line2 = ttp.slice(0 + line1Length, 0 + line1Length + line2Length);
+    line3 = ttp.slice(
+      0 + line1Length + line2Length,
+      0 + line1Length + line2Length + line3Length
+    );
   };
-
-
 </script>
 
-
-
 <div class="wrapper">
-  <textarea rows=3 cols=7 name="ttp" on:input={handleInput} />
-  <CharaterRow length={prop} value={value1} />
+  <textarea rows="3" cols="7" name="ttp" on:input={handleInput} />
+  <CharaterRow lineLength={line1Length} line={line1} />
+  <CharaterRow lineLength={line2Length} line={line2} />
+  <CharaterRow lineLength={line3Length} line={line3} />
 </div>
 
 <style>
@@ -25,18 +32,16 @@
     outline-color: white;
   }
 
-  .wrapper{
+  .wrapper {
     display: flex;
     flex-direction: row;
-    
   }
-  .wrapper{
+  .wrapper {
     flex: 1;
   }
   textarea {
-		position: absolute;
-		top: 0;
-		resize: none;
-	}
-
+    position: absolute;
+    bottom: 0;
+    resize: none;
+  }
 </style>
